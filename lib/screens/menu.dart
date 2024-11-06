8e1bb60e730374b036b56cec03ec4b9aa67d99a4
anandajoyyy22 <ananda.joy@ui.ac.id>
2024-11-06 15:17:55 +0700

    lab 7 done. waktu lab 6 salah ketik "lab 7 kelar" harusnya lab 6

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/widgets/mood_card.dart';
+// Impor drawer widget
+import 'package:mental_health_tracker/widgets/left_drawer.dart'; 
 
 class MyHomePage extends StatelessWidget {
   final String npm = '2206811190'; // NPM
@@ -9,6 +12,7 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Scaffold menyediakan struktur dasar halaman dengan appBar dan body.
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -19,7 +23,11 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
         backgroundColor: Theme.of(context).colorScheme.primary,
+        // Mengganti warna icon drawer menjadi putih
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
+      // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
+      drawer: const LeftDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -96,59 +104,3 @@ class InfoCard extends StatelessWidget {
   }
 }
 
-class ItemHomepage {
-  final String name;
-  final IconData icon;
-
-  ItemHomepage(this.name, this.icon);
-}
-
-class ItemCard extends StatelessWidget {
-  final ItemHomepage item;
-
-  const ItemCard(this.item, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Theme.of(context).colorScheme.secondary,
-      borderRadius: BorderRadius.circular(12),
-      child: InkWell(
-        onTap: () {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-        },
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-// List of items for the grid
-final List<ItemHomepage> items = [
-  ItemHomepage("Lihat Mood", Icons.mood),
-  ItemHomepage("Tambah Mood", Icons.add),
-  ItemHomepage("Logout", Icons.logout),
-];
